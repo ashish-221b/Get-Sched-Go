c93f9ef538b06222aeb0c3b370e2457436cf8dbf
RUPESH <rupesh@cse.iitb.ac.in>
2017-09-23 15:16:32 +0530

    Static Folder made, crispy-form and allauth app added from their git repo, updated settings.py

--- getSchedGo/getSchedGo/getSchedGo/settings.py
+++ getSchedGo/getSchedGo/getSchedGo/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #installed by cloning their git-repo
+    'crispy_forms',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',    
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# below this was added by rupesh to specify static file to django
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+    STATICFILES_DIRS = (
+        os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
+)
