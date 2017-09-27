99830264ca231a3172e0f5c26c654c00eb609e48
ASHISH MITTAL <ashishm@cse.iitb.ac.in>
2017-09-27 20:28:09 +0530

    migrating Timetable

--- getSchedGo/getSchedGo/Timetable/models.py
+++ getSchedGo/getSchedGo/Timetable/models.py
@@ -4,33 +4,37 @@ from django.conf import settings
 from profiles.models import profile
 # Create your models here.
 #Time Table for a Day
+Priority_Options = [('1','Normal'),('2','Preferred'), ('3','Important'), ('4','Indespensable')]
+Event_Timings = [('A','Duration Fixed'),('B','Duration and Timing Fixed'),('C',('Variable'))]
+Event_Type = [('A','Official Classes'), ('B','Study Acads'), ('C','Extra Study'), ('D','ExtraCurriculars'),('E','Misc.')]
 class DailySched(models.Model):
-    UserProfile = models.ForeignKey(profile, on_delete=models.CASCADE)
-    Active_day = models.DateField()
-    name = models.CharField(max_length=120)
-    def __str__(self):
+	UserProfile = models.ForeignKey(profile, on_delete=models.CASCADE)
+	Active_day = models.DateField()
+	name = models.CharField(max_length=50)
+	def __str__(self):
 		return self.name
 #Events
 class Event(object):
-    """docstring for Event."""
-    UserProfile = models.ForeignKey(profile, on_delete=models.CASCADE)
-    name = models.CharField(max_length=120)
-    Description = models.CharField(max_length=300,null=True,blank=True)
-    Venue = models.CharField(max_length=120,null=True,blank=True)
-    StartTime = models.DateTimeField(null=True,blank=True)
-    EndTime = models.DateTimeField(null=True,blank=True)
-    DeadLine = models.DateTimeField(null=True,blank=True)
-    Priority = models.CharField(max_length=120,null=True,blank=True)
-    Type = models.CharField(max_length=120,null=True,blank=True)
+	"""docstring for Event."""
+	UserProfile = models.ForeignKey(profile, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	Description = models.CharField(max_length=300,blank=True)
+	Venue = models.CharField(max_length=100,blank=True)
+	StartTime = models.DateTimeField(null=True,blank=True)
+	EndTime = models.DateTimeField(null=True,blank=True)
+	DeadLine = models.DateTimeField(null=True,blank=True)
+	Priority = models.CharField(max_length=25,blank=True,choices=Priority_Options,default='1')
+	Type = models.CharField(max_length=25,blank=True,choices=Event_Type,default='')
+	TimeSettings = models.CharField(max_length=25,blank=True,choices=Event_Timings,default='C')
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 #Slots of the day
 class Slots(models.Model):
-    UserProfile = models.ForeignKey(profile, on_delete=models.CASCADE)
-    Day_Sched = models.ForeignKey(DailySched, on_delete=models.CASCADE)
-    StartTime = models.DateTimeField(null=False,blank=False)
-    EndTime = models.DateTimeField(null=False,blank=False)
-    SlotNum= models.IntegerField(null=False,blank=False)
-    def __str__(self):
-        return self.SlotNum
+	UserProfile = models.ForeignKey(profile, on_delete=models.CASCADE)
+	Day_Sched = models.ForeignKey(DailySched, on_delete=models.CASCADE)
+	StartTime = models.DateTimeField(null=False,blank=False)
+	EndTime = models.DateTimeField(null=False,blank=False)
+	SlotNum= models.IntegerField(null=False,blank=False)
+	def __str__(self):
+		return self.SlotNum
--- getSchedGo/getSchedGo/getSchedGo/settings.py
+++ getSchedGo/getSchedGo/getSchedGo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles',
+    'Timetable',
     'crispy_forms',
     'django.contrib.sites',
     'allauth',
