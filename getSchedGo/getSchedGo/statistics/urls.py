e9fbde1e103078322c649db3ded48e46f2e71427
RUPESH <rupesh@cse.iitb.ac.in>
2017-10-15 20:51:05 +0530

    Modified Statistics view

--- getSchedGo/getSchedGo/statistics/urls.py
+++ getSchedGo/getSchedGo/statistics/urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     url(r"^list/$", views.EventBeforeDate, name="EventBeforeDate"),
     url(r"^donelist/$", views.CompletedList, name="CompletedList"),
     url(r"^(?P<pk>[0-9]+)/comp/$", views.MarkItCompleted, name="MarkItCompleted"),
-    # url(r"^create/$", views.CreateEvent, name="CreateEvent"),
-    # url(r"^(?P<pk>[0-9]+)/edit$", views.CreateEvent, name="EditEvent"),
-    # url(r"^(?P<pk>[0-9]+)/delete$", views.DeleteEvent, name="DeleteEvent"),
-    # url(r"^(?P<pk>[0-9]+)/sort$", views.EventList, name="SortEvent"),
-    # url(r'^today/$',views.Schedules, name = "Schedule"),
 ]
